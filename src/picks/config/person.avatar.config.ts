c97a4a922529e9d3a96c1b2d4334632b3b1f60cb
Mojtaba Asadollahzadeh <mgblow@gmail.com>
2025-11-09 23:41:12 +0330

    feat: pick item prices

--- src/picks/config/person.avatar.config.ts
+++ src/picks/config/person.avatar.config.ts
@@ -1,4 +1,4 @@
-// src/config/person.avatar.config.ts
+// src/config/person.avatar.config.ts // keeps 40% free
 
 // Avatar Styles
 export const avatarStyles = [
@@ -9,40 +9,40 @@ export const avatarStyles = [
 // Top Types
 export const topTypes = [
   { value: 'NoHair', label: 'بدون مو', isPaid: false, price: 0 },
-  { value: 'Eyepatch', label: 'چشم‌بند', isPaid: true, price: 20000 },
+  { value: 'Eyepatch', label: 'چشم‌بند', isPaid: true, price: 25000 },
   { value: 'Hat', label: 'کلاه', isPaid: false, price: 0 },
-  { value: 'Hijab', label: 'حجاب', isPaid: true, price: 15000 },
-  { value: 'Turban', label: 'عمامه', isPaid: true, price: 18000 },
+  { value: 'Hijab', label: 'حجاب', isPaid: true, price: 25000 },
+  { value: 'Turban', label: 'عمامه', isPaid: true, price: 25000 },
   { value: 'WinterHat1', label: 'کلاه زمستانی ۱', isPaid: false, price: 0 },
   { value: 'WinterHat2', label: 'کلاه زمستانی ۲', isPaid: false, price: 0 },
-  { value: 'WinterHat3', label: 'کلاه زمستانی ۳', isPaid: true, price: 12000 },
-  { value: 'WinterHat4', label: 'کلاه زمستانی ۴', isPaid: true, price: 15000 },
-  { value: 'LongHairBigHair', label: 'موی بلند حجیم', isPaid: true, price: 30000 },
+  { value: 'WinterHat3', label: 'کلاه زمستانی ۳', isPaid: true, price: 20000 },
+  { value: 'WinterHat4', label: 'کلاه زمستانی ۴', isPaid: true, price: 20000 },
+  { value: 'LongHairBigHair', label: 'موی بلند حجیم', isPaid: true, price: 35000 },
   { value: 'LongHairBob', label: 'بوب', isPaid: false, price: 0 },
-  { value: 'LongHairBun', label: 'دم اسبی', isPaid: true, price: 10000 },
-  { value: 'LongHairCurly', label: 'فر', isPaid: true, price: 12000 },
+  { value: 'LongHairBun', label: 'دم اسبی', isPaid: true, price: 30000 },
+  { value: 'LongHairCurly', label: 'فر', isPaid: true, price: 25000 },
   { value: 'LongHairCurvy', label: 'موج دار', isPaid: false, price: 0 },
-  { value: 'LongHairDreads', label: 'رشته‌ای', isPaid: true, price: 20000 },
-  { value: 'LongHairFrida', label: 'فریدا', isPaid: true, price: 25000 },
-  { value: 'LongHairFro', label: 'افرو', isPaid: false, price: 0 },
-  { value: 'LongHairFroBand', label: 'افرو با باند', isPaid: true, price: 15000 },
+  { value: 'LongHairDreads', label: 'رشته‌ای', isPaid: true, price: 35000 },
+  { value: 'LongHairFrida', label: 'فریدا', isPaid: true, price: 45000 },
+  { value: 'LongHairFro', label: 'افرو', isPaid: true, price: 35000 },
+  { value: 'LongHairFroBand', label: 'افرو با باند', isPaid: false, price: 0 },
   { value: 'LongHairNotTooLong', label: 'نیمه بلند', isPaid: false, price: 0 },
-  { value: 'LongHairShavedSides', label: 'طرفین تراشیده', isPaid: true, price: 10000 },
-  { value: 'LongHairMiaWallace', label: 'میا والاس', isPaid: true, price: 25000 },
+  { value: 'LongHairShavedSides', label: 'طرفین تراشیده', isPaid: true, price: 20000 },
+  { value: 'LongHairMiaWallace', label: 'میا والاس', isPaid: true, price: 45000 },
   { value: 'LongHairStraight', label: 'صاف', isPaid: false, price: 0 },
-  { value: 'LongHairStraight2', label: 'صاف ۲', isPaid: false, price: 0 },
+  { value: 'LongHairStraight2', label: 'صاف ۲', isPaid: true, price: 15000 },
   { value: 'LongHairStraightStrand', label: 'رشته صاف', isPaid: false, price: 0 },
-  { value: 'ShortHairDreads01', label: 'رشته کوتاه ۱', isPaid: false, price: 0 },
-  { value: 'ShortHairDreads02', label: 'رشته کوتاه ۲', isPaid: true, price: 10000 },
-  { value: 'ShortHairFrizzle', label: 'مجعد', isPaid: false, price: 0 },
-  { value: 'ShortHairShaggyMullet', label: 'شگی مولت', isPaid: true, price: 15000 },
-  { value: 'ShortHairShortCurly', label: 'فر کوتاه', isPaid: false, price: 0 },
+  { value: 'ShortHairDreads01', label: 'رشته کوتاه ۱', isPaid: true, price: 20000 },
+  { value: 'ShortHairDreads02', label: 'رشته کوتاه ۲', isPaid: true, price: 20000 },
+  { value: 'ShortHairFrizzle', label: 'مجعد', isPaid: true, price: 25000 },
+  { value: 'ShortHairShaggyMullet', label: 'شگی مولت', isPaid: false, price: 0 },
+  { value: 'ShortHairShortCurly', label: 'فر کوتاه', isPaid: true, price: 15000 },
   { value: 'ShortHairShortFlat', label: 'صاف کوتاه', isPaid: false, price: 0 },
   { value: 'ShortHairShortRound', label: 'گرد کوتاه', isPaid: false, price: 0 },
   { value: 'ShortHairShortWaved', label: 'موج کوتاه', isPaid: false, price: 0 },
   { value: 'ShortHairSides', label: 'طرفین', isPaid: false, price: 0 },
-  { value: 'ShortHairTheCaesar', label: 'سزار', isPaid: false, price: 0 },
-  { value: 'ShortHairTheCaesarSidePart', label: 'سزار با خط', isPaid: true, price: 12000 }
+  { value: 'ShortHairTheCaesar', label: 'سزار', isPaid: true, price: 25000 },
+  { value: 'ShortHairTheCaesarSidePart', label: 'سزار با خط', isPaid: true, price: 30000 }
 ];
 
 // Accessories Types
@@ -50,58 +50,58 @@ export const accessoriesTypes = [
   { value: 'Blank', label: 'هیچ‌کدام', isPaid: false, price: 0 },
   { value: 'Kurt', label: 'کلاه کورت', isPaid: true, price: 15000 },
   { value: 'Prescription01', label: 'عینک ۱', isPaid: false, price: 0 },
-  { value: 'Prescription02', label: 'عینک ۲', isPaid: true, price: 10000 },
-  { value: 'Round', label: 'عینک گرد', isPaid: true, price: 12000 },
-  { value: 'Sunglasses', label: 'عینک آفتابی', isPaid: true, price: 20000 },
-  { value: 'Wayfarers', label: 'عینک ویفرز', isPaid: true, price: 15000 }
+  { value: 'Prescription02', label: 'عینک ۲', isPaid: true, price: 20000 },
+  { value: 'Round', label: 'عینک گرد', isPaid: true, price: 25000 },
+  { value: 'Sunglasses', label: 'عینک آفتابی', isPaid: true, price: 30000 },
+  { value: 'Wayfarers', label: 'عینک ویفرز', isPaid: false, price: 0 }
 ];
 
 // Hair Colors
 export const hairColors = [
   { value: 'Auburn', label: 'عنابی', hex: '#A55728', isPaid: true, price: 10000 },
   { value: 'Black', label: 'مشکی', hex: '#2C1B18', isPaid: false, price: 0 },
-  { value: 'Blonde', label: 'بلوند', hex: '#B58143', isPaid: true, price: 8000 },
-  { value: 'BlondeGolden', label: 'بلوند طلایی', hex: '#D6B370', isPaid: true, price: 12000 },
+  { value: 'Blonde', label: 'بلوند', hex: '#B58143', isPaid: true, price: 15000 },
+  { value: 'BlondeGolden', label: 'بلوند طلایی', hex: '#D6B370', isPaid: true, price: 20000 },
   { value: 'Brown', label: 'قهوه‌ای', hex: '#724133', isPaid: false, price: 0 },
   { value: 'BrownDark', label: 'قهوه‌ای تیره', hex: '#4A312C', isPaid: false, price: 0 },
-  { value: 'PastelPink', label: 'صورتی پاستلی', hex: '#F59797', isPaid: true, price: 20000 },
-  { value: 'Platinum', label: 'پلاتینیوم', hex: '#ECDCBF', isPaid: true, price: 25000 },
+  { value: 'PastelPink', label: 'صورتی پاستلی', hex: '#F59797', isPaid: true, price: 25000 },
+  { value: 'Platinum', label: 'پلاتینیوم', hex: '#ECDCBF', isPaid: true, price: 30000 },
   { value: 'Red', label: 'قرمز', hex: '#C93305', isPaid: true, price: 15000 },
-  { value: 'SilverGray', label: 'نقره‌ای', hex: '#E8E1E1', isPaid: true, price: 18000 }
+  { value: 'SilverGray', label: 'نقره‌ای', hex: '#E8E1E1', isPaid: false, price: 0 }
 ];
 
 // Facial Hair Types
 export const facialHairTypes = [
   { value: 'Blank', label: 'هیچ‌کدام', isPaid: false, price: 0 },
-  { value: 'BeardMedium', label: 'ریش متوسط', isPaid: true, price: 10000 },
+  { value: 'BeardMedium', label: 'ریش متوسط', isPaid: true, price: 15000 },
   { value: 'BeardLight', label: 'ریش کم', isPaid: false, price: 0 },
   { value: 'BeardMajestic', label: 'ریش مجلل', isPaid: true, price: 25000 },
   { value: 'MoustacheFancy', label: 'سبیل فانتزی', isPaid: true, price: 15000 },
-  { value: 'MoustacheMagnum', label: 'سبیل بزرگ', isPaid: false, price: 0 }
+  { value: 'MoustacheMagnum', label: 'سبیل بزرگ', isPaid: true, price: 20000 }
 ];
 
 // Facial Hair Colors
 export const facialHairColors = [
-  { value: 'Auburn', label: 'عنابی', hex: '#A55728', isPaid: true, price: 8000 },
+  { value: 'Auburn', label: 'عنابی', hex: '#A55728', isPaid: true, price: 10000 },
   { value: 'Black', label: 'مشکی', hex: '#2C1B18', isPaid: false, price: 0 },
-  { value: 'Blonde', label: 'بلوند', hex: '#B58143', isPaid: true, price: 8000 },
-  { value: 'BlondeGolden', label: 'بلوند طلایی', hex: '#D6B370', isPaid: true, price: 12000 },
+  { value: 'Blonde', label: 'بلوند', hex: '#B58143', isPaid: true, price: 10000 },
+  { value: 'BlondeGolden', label: 'بلوند طلایی', hex: '#D6B370', isPaid: true, price: 15000 },
   { value: 'Brown', label: 'قهوه‌ای', hex: '#724133', isPaid: false, price: 0 },
   { value: 'BrownDark', label: 'قهوه‌ای تیره', hex: '#4A312C', isPaid: false, price: 0 },
   { value: 'Platinum', label: 'پلاتینیوم', hex: '#ECDCBF', isPaid: true, price: 15000 },
-  { value: 'Red', label: 'قرمز', hex: '#C93305', isPaid: true, price: 10000 }
+  { value: 'Red', label: 'قرمز', hex: '#C93305', isPaid: true, price: 20000 }
 ];
 
 // Clothe Types
 export const clotheTypes = [
-  { value: 'BlazerShirt', label: 'بلزر و پیراهن', isPaid: true, price: 25000 },
-  { value: 'BlazerSweater', label: 'بلزر و سوئیشرت', isPaid: true, price: 30000 },
-  { value: 'CollarSweater', label: 'سوئیشرت یقه دار', isPaid: true, price: 15000 },
-  { value: 'GraphicShirt', label: 'پیراهن گرافیکی', isPaid: false, price: 0 },
-  { value: 'Hoodie', label: 'هودی', isPaid: false, price: 0 },
-  { value: 'Overall', label: 'اورال', isPaid: true, price: 20000 },
+  { value: 'BlazerShirt', label: 'بلزر و پیراهن', isPaid: true, price: 30000 },
+  { value: 'BlazerSweater', label: 'بلزر و سوئیشرت', isPaid: true, price: 25000 },
+  { value: 'CollarSweater', label: 'سوئیشرت یقه دار', isPaid: false, price: 0 },
+  { value: 'GraphicShirt', label: 'پیراهن گرافیکی', isPaid: true, price: 20000 },
+  { value: 'Hoodie', label: 'هودی', isPaid: true, price: 25000 },
+  { value: 'Overall', label: 'اورال', isPaid: false, price: 0 },
   { value: 'ShirtCrewNeck', label: 'پیراهن یقه گرد', isPaid: false, price: 0 },
-  { value: 'ShirtScoopNeck', label: 'پیراهن یقه اسکوپ', isPaid: false, price: 0 },
+  { value: 'ShirtScoopNeck', label: 'پیراهن یقه اسکوپ', isPaid: true, price: 15000 },
   { value: 'ShirtVNeck', label: 'پیراهن یقه وی', isPaid: false, price: 0 }
 ];
 
@@ -109,54 +109,80 @@ export const clotheTypes = [
 export const clotheColors = [
   { value: 'Black', label: 'مشکی', hex: '#262E33', isPaid: false, price: 0 },
   { value: 'Blue01', label: 'آبی ۱', hex: '#65C9FF', isPaid: false, price: 0 },
-  { value: 'Blue02', label: 'آبی ۲', hex: '#5199E4', isPaid: false, price: 0 },
-  { value: 'Blue03', label: 'آبی ۳', hex: '#25557C', isPaid: true, price: 8000 },
+  { value: 'Blue02', label: 'آبی ۲', hex: '#5199E4', isPaid: true, price: 10000 },
+  { value: 'Blue03', label: 'آبی ۳', hex: '#25557C', isPaid: true, price: 15000 },
   { value: 'Gray01', label: 'خاکستری ۱', hex: '#E6E6E6', isPaid: false, price: 0 },
-  { value: 'Heather', label: 'هیثر', hex: '#3C4F5C', isPaid: true, price: 10000 },
-  { value: 'PastelRed', label: 'قرمز پاستلی', hex: '#FFAFB9', isPaid: true, price: 15000 },
-  { value: 'Red', label: 'قرمز', hex: '#FF5C5C', isPaid: true, price: 15000 },
+  { value: 'Gray02', label: 'خاکستری ۲', hex: '#929598', isPaid: false, price: 0 },
+  { value: 'Heather', label: 'هیثر', hex: '#3C4F5C', isPaid: true, price: 20000 },
+  { value: 'PastelBlue', label: 'آبی پاستلی', hex: '#B1E2FF', isPaid: true, price: 15000 },
+  { value: 'PastelGreen', label: 'سبز پاستلی', hex: '#A7FFC4', isPaid: true, price: 15000 },
+  { value: 'PastelOrange', label: 'نارنجی پاستلی', hex: '#FFDEB5', isPaid: true, price: 20000 },
+  { value: 'PastelRed', label: 'قرمز پاستلی', hex: '#FFAFB9', isPaid: true, price: 20000 },
+  { value: 'PastelYellow', label: 'زرد پاستلی', hex: '#FFFFB1', isPaid: false, price: 0 },
+  { value: 'Pink', label: 'صورتی', hex: '#FF488E', isPaid: true, price: 25000 },
+  { value: 'Red', label: 'قرمز', hex: '#FF5C5C', isPaid: true, price: 20000 },
   { value: 'White', label: 'سفید', hex: '#FFFFFF', isPaid: false, price: 0 }
 ];
 
 // Eye Types
 export const eyeTypes = [
   { value: 'Close', label: 'بسته', isPaid: false, price: 0 },
-  { value: 'Cry', label: 'گریه', isPaid: true, price: 8000 },
+  { value: 'Cry', label: 'گریه', isPaid: true, price: 20000 },
   { value: 'Default', label: 'پیش‌فرض', isPaid: false, price: 0 },
+  { value: 'Dizzy', label: 'سرگیجه', isPaid: true, price: 20000 },
+  { value: 'EyeRoll', label: 'چرخش چشم', isPaid: true, price: 25000 },
   { value: 'Happy', label: 'شاد', isPaid: false, price: 0 },
-  { value: 'Hearts', label: 'قلب', isPaid: true, price: 12000 },
-  { value: 'Wink', label: 'چشمک', isPaid: true, price: 10000 },
-  { value: 'Surprised', label: 'متعجب', isPaid: false, price: 0 }
+  { value: 'Hearts', label: 'قلب', isPaid: true, price: 30000 },
+  { value: 'Side', label: 'کناری', isPaid: false, price: 0 },
+  { value: 'Squint', label: 'نیمه بسته', isPaid: false, price: 0 },
+  { value: 'Surprised', label: 'متعجب', isPaid: true, price: 20000 },
+  { value: 'Wink', label: 'چشمک', isPaid: false, price: 0 },
+  { value: 'WinkWacky', label: 'چشمک عجیب', isPaid: true, price: 25000 }
 ];
 
 // Eyebrow Types
 export const eyebrowTypes = [
+  { value: 'Angry', label: 'عصبی', isPaid: false, price: 0 },
+  { value: 'AngryNatural', label: 'عصبی طبیعی', isPaid: true, price: 20000 },
   { value: 'Default', label: 'پیش‌فرض', isPaid: false, price: 0 },
-  { value: 'FlatNatural', label: 'صاف طبیعی', isPaid: false, price: 0 },
-  { value: 'RaisedExcited', label: 'بالا رفته هیجان‌زده', isPaid: true, price: 8000 },
-  { value: 'SadConcerned', label: 'ناراحت نگران', isPaid: false, price: 0 },
-  { value: 'UpDownNatural', label: 'بالا پایین طبیعی', isPaid: true, price: 10000 }
+  { value: 'DefaultNatural', label: 'طبیعی پیش‌فرض', isPaid: false, price: 0 },
+  { value: 'FlatNatural', label: 'صاف طبیعی', isPaid: true, price: 15000 },
+  { value: 'RaisedExcited', label: 'بالا رفته هیجان‌زده', isPaid: true, price: 25000 },
+  { value: 'RaisedExcitedNatural', label: 'بالا رفته هیجان‌زده طبیعی', isPaid: true, price: 20000 },
+  { value: 'SadConcerned', label: 'ناراحت نگران', isPaid: true, price: 15000 },
+  { value: 'SadConcernedNatural', label: 'ناراحت نگران طبیعی', isPaid: false, price: 0 },
+  { value: 'UnibrowNatural', label: 'اتصال ابرو طبیعی', isPaid: true, price: 10000 },
+  { value: 'UpDown', label: 'بالا پایین', isPaid: false, price: 0 },
+  { value: 'UpDownNatural', label: 'بالا پایین طبیعی', isPaid: true, price: 20000 }
 ];
 
 // Mouth Types
 export const mouthTypes = [
+  { value: 'Concerned', label: 'نگران', isPaid: false, price: 0 },
   { value: 'Default', label: 'پیش‌فرض', isPaid: false, price: 0 },
+  { value: 'Disbelief', label: 'ناباوری', isPaid: true, price: 20000 },
+  { value: 'Eating', label: 'در حال غذا خوردن', isPaid: true, price: 25000 },
+  { value: 'Grimace', label: 'اخم', isPaid: false, price: 0 },
+  { value: 'Sad', label: 'ناراحت', isPaid: false, price: 0 },
+  { value: 'ScreamOpen', label: 'فریاد باز', isPaid: true, price: 25000 },
+  { value: 'Serious', label: 'جدی', isPaid: true, price: 15000 },
   { value: 'Smile', label: 'لبخند', isPaid: false, price: 0 },
-  { value: 'Tongue', label: 'زبان', isPaid: true, price: 8000 },
-  { value: 'ScreamOpen', label: 'فریاد باز', isPaid: true, price: 10000 },
-  { value: 'Twinkle', label: 'برق', isPaid: true, price: 12000 }
+  { value: 'Tongue', label: 'زبان', isPaid: true, price: 20000 },
+  { value: 'Twinkle', label: 'برق', isPaid: true, price: 25000 },
+  { value: 'Vomit', label: 'استفراغ', isPaid: true, price: 20000 }
 ];
 
 // Skin Colors
 export const skinColors = [
-  { value: 'Tanned', label: 'برنزه', hex: '#FD9841', isPaid: true, price: 10000 },
+  { value: 'Tanned', label: 'برنزه', hex: '#FD9841', isPaid: false, price: 0 },
   { value: 'Yellow', label: 'زرد', hex: '#F8D25C', isPaid: false, price: 0 },
-  { value: 'Pale', label: 'سفید', hex: '#FFDBB4', isPaid: false, price: 0 },
+  { value: 'Pale', label: 'رنگ پریده', hex: '#FFDBB4', isPaid: false, price: 0 },
   { value: 'Light', label: 'روشن', hex: '#EDB98A', isPaid: false, price: 0 },
-  { value: 'Brown', label: 'قهوه‌ای', hex: '#D08B5B', isPaid: false, price: 0 },
-  { value: 'DarkBrown', label: 'قهوه‌ای تیره', hex: '#AE5D29', isPaid: true, price: 8000 },
-  { value: 'Black', label: 'مشکی', hex: '#614335', isPaid: false, price: 0 }
+  { value: 'Brown', label: 'قهوه‌ای', hex: '#A55728', isPaid: false, price: 0 },
+  { value: 'DarkBrown', label: 'قهوه‌ای تیره', hex: '#614335', isPaid: false, price: 0 },
+  { value: 'Black', label: 'مشکی', hex: '#2C1B18', isPaid: false, price: 0 }
 ];
+
 
 export const avatarPicks = {
   avatarStyles,
